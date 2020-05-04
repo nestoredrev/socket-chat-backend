83fe930345fd36954d10d8e299a7dfb3edfc6a0b
nestor <nestor.edrev@gmail.com>
2020-05-04 13:17:50 +0200

    APIREST con Heroku y Mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -2,7 +2,14 @@
     Configuracion del entorno de desarrollo y produccion
 */
 
+/*
+	Con el add-ons de mLab que ofrede Heroku genera automaticamente
+	MONGODB_URI que esta definida en las variables de configuracion de Heroku.
+	Para ver las variables de configuracion de Heroku: heroku config
+*/
+
 module.exports = {
 	port: process.env.PORT || 3000,
 	db: process.env.MONGODB_URI || 'mongodb://localhost:27017/cafe'
 }
+
